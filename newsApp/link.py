86384e210a54db9dd92f5acdafe1370502ab925f
adityabansal <adityabansal.in@gmail.com>
2014-07-15 11:14:50 +0530

    adding class for web page links

---
+++ newsApp/link.py
@@ -0,0 +1,27 @@
+from newsApp.dbItem import DbItem
+import urllib2
+
+def getIdentifierUrl(url):
+  """
+  Returns a unique identifier for the link.
+  Someimes 2 different links can actually point to the same web page
+  due to redirects etc.
+  """
+
+  result = urllib2.urlopen(url)
+  return result.geturl()
+
+class Link(DbItem):
+  """
+  Represents a link to a webPage.
+
+  Each wlink consists of a unique identifier(the url)
+  and a set of tags(key-value pairs).
+  """
+
+  def __init__(self, id, tags=None):
+    """
+    Instantiates a link object representing a link to a web page.
+    """
+
+    DbItem.__init__(self, getIdentifierUrl(id), tags);
---
+++ tests/link_test.py
@@ -0,0 +1,23 @@
+from newsApp.link import Link
+import unittest
+import os
+
+def getLocalFileUrl(relativePath):
+    """
+    Get the url of local file given it's relative path.
+    """
+
+    return "file://" + os.path.abspath(relativePath)
+
+class LinkTests(unittest.TestCase):
+
+    def testCreateLink(self):
+        testLink = Link(getLocalFileUrl("tests/testData/testHtml.html"))
+
+    def testCreateDupLinks(self):
+        link1 = Link("http://www.independent.co.uk/life-style/gadgets-and-tech/news/chinese-blamed-for-gmail-hacking-2292113.html")
+        link2 = Link("http://www.independent.co.uk/life-style/gadgets-and-tech/news/2292113.html")
+        self.failUnless(link1.id == link2.id);
+
+if __name__ == '__main__':
+    unittest.main()
