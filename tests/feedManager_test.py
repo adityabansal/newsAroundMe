ee9f6926bd9563b78a7b523a4fb16e07bb6ecf62
adityabansal <adityabansal.in@gmail.com>
2014-07-20 13:22:51 +0530

    added a test

--- tests/feedManager_test.py
+++ tests/feedManager_test.py
@@ -21,3 +21,28 @@ class FeedManagerTests(unittest.TestCase):
         # delete feed. trying to get feed should raise an exception
         testFeedManager.delete(testFeed.id)
         self.assertRaises(Exception, testFeedManager.get, testFeed.id)
+
+    def testPutFeedTwice(self):
+        """
+        Puttng a feed twice possibly with different tags should work.
+        """
+
+        testFeedManager = FeedManager()
+        testFeed = Feed(
+            'testFeedName',
+            { 'tag1' : 'value1', 'tag2' : 'value2' })
+
+        # put the feed
+        testFeedManager.put(testFeed)
+
+        # add a tag and put the feed again
+        testFeed.tags['tag3'] = 'value3'
+        testFeedManager.put(testFeed)
+
+        # get the feed and validate it's same as one you put the 2nd time
+        retrievedFeed = testFeedManager.get(testFeed.id)
+        self.failUnless(retrievedFeed.tags == testFeed.tags)
+        self.failUnless(retrievedFeed.id == testFeed.id)
+
+        # Cleanup - delete feed.
+        testFeedManager.delete(testFeed.id)
