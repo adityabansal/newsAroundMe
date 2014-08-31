182862c4eb1f00f33a77805ab1b12242e0753154
adityabansal <adityabansal.in@gmail.com>
2014-08-31 18:28:03 +0530

    Tool to populate feeds table from an xml file

---
+++ tests/populateTestFeeds.py
@@ -0,0 +1,21 @@
+import xml.etree.ElementTree as ET
+import sys
+
+from newsApp.feed import Feed
+from newsApp.feedManager import FeedManager
+
+def populateFeeds():
+    tree = ET.parse(sys.argv[1])
+    root = tree.getroot()
+
+    for child in root:
+        feedTags = child.attrib
+        feedId = feedTags['id'];
+        feedTags.pop('key', None);
+
+        feed = Feed(feedId, feedTags);
+        feedManager = FeedManager()
+        feedManager.put(feed);
+
+if __name__ == '__main__':
+    populateFeeds()
