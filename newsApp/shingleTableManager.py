6807a167f9398abe887d33bd9b4598d1821ef913
adityabansal <adityabansal.in@gmail.com>
2015-06-06 23:43:09 +0530

    Class to store/query shingle-docId pairs from a database

---
+++ newsApp/shingleTableManager.py
@@ -0,0 +1,99 @@
+import os
+import time
+
+from boto.dynamodb2.table import Table
+from boto.dynamodb2.fields import HashKey, RangeKey, GlobalAllIndex
+
+from dbhelper import *
+
+class ShingleTableManager:
+    """
+    Manage shingle-docid pairs stored on AWS dynamo db database.
+
+    Contains functions for CRUD on table, adding and querying entries etc.
+    """
+
+    def __init__(self):
+        """
+        Instantiates a new instance of DbItemManager class
+        """
+
+        self.tableConnString = os.environ['SHINGLETABLE_CONNECTIONSTRING'];
+
+    def __getTable(self):
+        """
+        Get the shingle table.
+        """
+
+        shingleTableConnectionParams = parseConnectionString(
+            self.tableConnString);
+
+        return Table(
+            shingleTableConnectionParams['name'],
+            connection = getDbConnection(shingleTableConnectionParams));
+
+    def createFreshTable(self):
+        """
+        Create a fresh empty shingle table.
+        """
+
+        # delete existing table if it exists
+        try:
+            self.__getTable().delete();
+            time.sleep(5)
+        except:
+            pass;# do nothing. Maybe there was no existing table
+
+        # create new table
+        shingleTableConnectionParams = parseConnectionString(
+            self.tableConnString);
+        return Table.create(
+            shingleTableConnectionParams['name'],
+            schema = [
+                HashKey('shingle'),
+                RangeKey('docId')
+            ], throughput = {
+                'read': 1,
+                'write': 1,
+            }, global_indexes = [
+                GlobalAllIndex('docIdIndex', parts = [
+                    HashKey('docId'),
+                    RangeKey('shingle')
+                ],
+                throughput = {
+                    'read': 1,
+                    'write': 1,
+                })
+            ],
+            connection = getDbConnection(shingleTableConnectionParams))
+
+    def addEntries(self, docId, shingles):
+        """
+        Add a entries in shingles table for shingles and docId passed.
+        """
+
+        shingleTable = self.__getTable()
+        with shingleTable.batch_write() as batch:
+            for shingle in shingles:
+                batch.put_item(data={
+                    'docId': docId,
+                    'shingle': shingle})
+
+    def queryByShingle(self, shingle):
+        """
+        Retrieve list of docId's for the passed shingle.
+        """
+
+        shingleTable = self.__getTable()
+        return (row['docId'] for row in shingleTable.query_2(
+            shingle_eq = shingle))
+
+    def queryByDocId(self, docId):
+        """
+        Retrieve list of shingles for the passed docId.
+        """
+
+        shingleTable = self.__getTable()
+        return (row['shingle'] for row in shingleTable.query_2(
+            docId_eq = docId,
+            index = 'docIdIndex'))
---
+++ tests/shingleTableManager_test.py
@@ -0,0 +1,27 @@
+import time
+import unittest
+
+from newsApp.shingleTableManager import ShingleTableManager
+
+class ShingleTableManagerTests(unittest.TestCase):
+
+    def testCreateTableAndUse(self):
+        testShingleTableManager = ShingleTableManager()
+
+        # create the table
+        testShingleTableManager.createFreshTable();
+
+        # wait for table to get created and add entries
+        time.sleep(10);
+        testShingleTableManager.addEntries('testDoc1', ['a', 'b', 'c']);
+        testShingleTableManager.addEntries('testDoc2', ['a', 'b', 'd']);
+
+        #query by shingle
+        result = testShingleTableManager.queryByShingle('a')
+        self.failUnless(result, ['testDoc1', 'testDoc2'])
+        result = testShingleTableManager.queryByShingle('d')
+        self.failUnless(result, ['testDoc2'])
+
+        #query by docId
+        result = testShingleTableManager.queryByDocId('testDoc1')
+        self.failUnless(result, ['a', 'b', 'c'])
