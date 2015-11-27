a3b8430de1695e4bba6b70ca79a777201587798a
Aditya Bansal <adityabansal.in@gmail.com>
2015-11-27 14:52:48 +0530

    always process rss entries in which pubDate could not be passed

--- newsApp/rssProcessor.py
+++ newsApp/rssProcessor.py
@@ -92,7 +92,8 @@ def processFeed(jobId, feedId):
   # get all feed entries since last poll time
   parsedFeed = feedparser.parse(feed.tags[FEEDTAG_URL])
   newEntries = [entry for entry in parsedFeed.entries
-               if entry.published_parsed > time.gmtime(lastPollTime)]
+               if entry.published_parsed > time.gmtime(lastPollTime) or \
+                 entry.published_parsed is None]
   logger.info("Got %i new entries. %s", len(newEntries), feedAndJobId)
 
   # for each entry add link in link database and a process link job
