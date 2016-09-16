b8f34760ae9222b49d9fe448097c49a9df2599e7
Aditya Bansal <adityabansal.in@gmail.com>
2016-09-16 18:30:50 +0530

    supress logs from memcache

--- newsApp/loggingHelper.py
+++ newsApp/loggingHelper.py
@@ -13,4 +13,6 @@ def InitLogging():
     # suppress all logs except critical ones from boto
     logging.getLogger('boto').setLevel(logging.CRITICAL)
 
+    logging.getLogger('protocol').setLevel(logging.ERROR)
+
     logging.captureWarnings(True)
