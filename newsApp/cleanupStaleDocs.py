e6ce3ad2f515584c2e0ce7737f699f43f63172e8
Aditya Bansal <adityabansal.in@gmail.com>
2016-09-16 13:48:07 +0530

    Also put cleanup doc job when deleting doc.

--- newsApp/cleanupStaleDocs.py
+++ newsApp/cleanupStaleDocs.py
@@ -1,5 +1,8 @@
+from constants import *
 from docManager import DocManager
 from loggingHelper import *
+from minerJobManager import MinerJobManager
+from workerJob import WorkerJob
 
 InitLogging()
 
@@ -10,11 +13,18 @@ def cleanupStaleDocs():
     """
 
     docManager = DocManager()
+    jobManager = MinerJobManager()
     
     logging.info("Getting stale docs.")
     staleDocKeys = docManager.getStaleDocKeys()
 
     for docKey in staleDocKeys:
+        job = WorkerJob(JOB_CLEANUPDOC, { JOBARG_CLEANUPDOC_DOCID : docKey})
+        jobManager.enqueueJob(job)
+        logging.info(
+            "Put cleanup doc job for docId: %s. Job id: %s",
+            docKey,
+            job.jobId)
         logging.info("Deleting doc with key: %s", docKey);
         docManager.delete(docKey);
         logging.info("Deleted doc with key: %s", docKey);
