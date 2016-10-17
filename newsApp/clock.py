c254b53afc86055faad679c821ce04f0dfdda7d4
adityabansal <adityabansal.in@gmail.com>
2016-10-17 09:50:17 +0530

    Pushing feed jobs more frequently

--- newsApp/clock.py
+++ newsApp/clock.py
@@ -5,28 +5,38 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from pushFeedJobs import pushFeedJobs
 from startClustering import startIncrementalClustering
 from pushClusterJob import pushClusterJob
+from cleanupStaleLinks import cleanupStaleLinks
+from cleanupStaleDocs import cleanupStaleDocs
 
 now = datetime.datetime.now()
 clusteringInterval = 10
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes =8, start_date = now)
+@sched.scheduled_job('interval', minutes = 2, start_date = now)
 def pushFeedJobs_job():
     pushFeedJobs()
 
 @sched.scheduled_job(
 	'interval',
 	minutes =clusteringInterval,
-	start_date = now + datetime.timedelta(minutes = 2))
+	start_date = now + datetime.timedelta(minutes = 3))
 def startClustering_job():
 	startIncrementalClustering()
 
 @sched.scheduled_job(
 	'interval',
 	minutes = clusteringInterval,
-	start_date = now + datetime.timedelta(minutes = 4))
+	start_date = now + datetime.timedelta(minutes = 5))
 def pushClusterJob_job():
 	pushClusterJob()
+
+@sched.scheduled_job('interval', minutes = 60, start_date = now)
+def cleanupStaleLinks_job():
+    cleanupStaleLinks()
+
+@sched.scheduled_job('interval', minutes = 60, start_date = now)
+def cleanupStaleDocs_job():
+    cleanupStaleDocs()
 
 sched.start()
