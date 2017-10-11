fa4d87a5e6bfc6cff14c291da168030536320757
adityabansal <adityabansal.in@gmail.com>
2017-10-11 08:54:06 +0530

    Ensure selenium driver gets closed in case of errors getting the page

--- newsApp/webPageLoader.py
+++ newsApp/webPageLoader.py
@@ -15,13 +15,16 @@ def loadPageAndGetHtml(url):
     driver = webdriver.PhantomJS(executable_path='node_modules/phantomjs-prebuilt/bin/phantomjs')
     driver.implicitly_wait(30)
     driver.set_page_load_timeout(30)
-    driver.get(url)
-    pageHtml = driver.page_source;
+
     try:
-      driver.service.process.send_signal(signal.SIGTERM)
-      driver.quit()
-    except:
-      pass;
+      driver.get(url)
+      pageHtml = driver.page_source;
+    finally:
+      try:
+        driver.service.process.send_signal(signal.SIGTERM)
+        driver.quit()
+      except:
+        pass;
   except:
     logger.warning("Could not load page with url %s through selenium", url);
     pass;
