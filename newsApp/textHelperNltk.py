07b4b27013ac940d93fa88ad7427847270e955ff
adityabansal <adityabansal.in@gmail.com>
2018-04-01 09:12:12 +0530

    Text Helper changes to replace words with digits

--- newsApp/textHelperNltk.py
+++ newsApp/textHelperNltk.py
@@ -30,11 +30,40 @@ def hasNumbers(inputString):
 def getSentences(text):
     return nltk.sent_tokenize(text)
 
+def getDigitForToken(token):
+    digitsDict = {
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9',
+        'ten': '10',
+        'eleven': '11',
+        'twelve': '12',
+        'thirteen': '13',
+        'fourteen': '14',
+        'fifteen': '15',
+        'sixteen': '16',
+        'seventeen': '17',
+        'eighteen': '18',
+        'nineteen': '19'
+    }
+
+    return digitsDict.get(token, token)
+
+def replaceWordTokensWithDigits(tokens):
+    return [getDigitForToken(token) for token in tokens]
+
 def getTokens(text):
     text = removeNonAsciiChars(text);
     lowers = text.lower()
     no_punctuation = _removePuntuation(lowers)
     tokens = nltk.word_tokenize(no_punctuation)
+    tokens = replaceWordTokensWithDigits(tokens)
     return tokens
 
 def getStemmedTokens(text):
