fa33e5633f232354ac463d6a41e327b2502d3754
adityabansal <adityabansal.in@gmail.com>
2019-06-02 19:00:27 +0530

    Optimiziing the storage cost of processed clusters + storing them in cache

--- newsApp/processedClusterStore.py
+++ newsApp/processedClusterStore.py
@@ -6,6 +6,7 @@ from boto.s3.connection import S3Connection
 from boto.exception import S3ResponseError
 from boto.s3.key import Key
 
+from cachingHelper import getCache
 from cluster import Cluster
 from dbhelper import parseConnectionString, getS3Connection
 
@@ -18,12 +19,18 @@ class ProcessedClusterStore:
 
   def __init__(self):
     self.bucketConnString = os.environ['PROCESSEDCLUSTERS_BUCKET']
+    self.cache = getCache()
+    self.__cacheExpiry= 600
+    self.__cacheKeyPrefix = 'pc'
 
   def __getBucket(self):
     bucketConnParams = parseConnectionString(self.bucketConnString)
     conn = getS3Connection(self.bucketConnString)
 
     return conn.get_bucket(bucketConnParams['bucketName'], validate=False)
+
+  def __getCacheKey(self, clusterId):
+    return self.__cacheKeyPrefix + clusterId
 
   def getProcessedCluster(self, cluster):
     """
@@ -32,14 +39,24 @@ class ProcessedClusterStore:
     """
 
     clusterId = cluster.id
+
+    clusterFromCache =  self.cache.get(self.__getCacheKey(clusterId))
+    if clusterFromCache:
+      logging.info("Preprocessed cluster found in cache for: " + clusterId)
+
+      preProcessedCluster = Cluster([])
+      preProcessedCluster.deserializeFromString(clusterFromCache)
+      return preProcessedCluster
+
     try:
       k = Key(self.__getBucket())
       k.key = clusterId
       keyContents = k.get_contents_as_string()
-      logging.info("Preprocessed cluster found for: " + clusterId)
+      logging.info("Preprocessed cluster found in S3 for: " + clusterId)
 
       preProcessedCluster = Cluster([])
       preProcessedCluster.deserializeFromString(keyContents)
+      self.cache.set(self.__getCacheKey(clusterId), keyContents, self.__cacheExpiry)
       return preProcessedCluster
     except S3ResponseError:
       logging.info("Preprocessed cluster not found for: " + clusterId)
@@ -56,11 +73,20 @@ class ProcessedClusterStore:
 
     cluster.process()
     logging.info("Processed cluster with id: " + clusterId)
-    k.set_contents_from_string(cluster.serializeToString())
-    logging.info("Processed cluster saved for: " + clusterId)
+    serializedCluster = cluster.serializeToString()
+
+    # Save only large-size clusters in S3 as S3 PUT calls are expensive.
+    if len(cluster) > 4:
+      k.set_contents_from_string(serializedCluster)
+      logging.info("Processed cluster saved for: " + clusterId)
+
+    self.cache.set(self.__getCacheKey(clusterId), serializedCluster, self.__cacheExpiry)
     return cluster
 
   def deleteClusters(self, clusters):
     keysToDelete = [cluster.id for cluster in clusters]
+
+    self.cache.delete_multi(keysToDelete)
+
     bucket = self.__getBucket()
     bucket.delete_keys(keysToDelete)
